65fe14f309fb26238fa2cfd38f052cc7b947fe31
ElijahHHall <ehall851@gmail.com>
2018-03-28 08:57:24 -0600

    4th commit

--- blog.js
+++ blog.js
@@ -19,3 +19,23 @@ bloggersForm.addEventListener("submit", function(e){
 // }
 // document.getElementById("bloggers-form").innerHTML = "You have clicked the button " +
 // sessionStorage.clickcount + " time(s) in this session.";
+
+var btn = document.createElement("POST");
+var t = document.createTextNode("CLICK");
+
+btn.appendChild(t);
+document.body.appendChild(btn);
+
+
+
+
+// const form = document.getElementById("bloggersForm")
+
+// form.addEventListener("submit", function(e){
+	
+// 	e.preventDefault()
+// 	console.log("")
+// });
+
+
+// let inputValue =
